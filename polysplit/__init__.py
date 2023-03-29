d89c5d9d2e805e293fb43fa13b28b737790ffc83
r1p71d3 <dz2428@columbia.edu>
2023-03-28 21:03:48 -0400

    black init:

--- polysplit/__init__.py
+++ polysplit/__init__.py
@@ -1,8 +1,15 @@
-from .polysplit import polysplit_main, generate_points_within_polygon, \
-    shortest_path_distance, plot_polygon_and_regions, shapely_point_to_tuple
+from .polysplit import (
+    polysplit_main,
+    generate_points_within_polygon,
+    shortest_path_distance,
+    plot_polygon_and_regions,
+    shapely_point_to_tuple,
+)
 
-__all__ = ['polysplit_main',
-           'generate_points_within_polygon',
-           'shortest_path_distance',
-           'plot_polygon_and_regions',
-           'shapely_point_to_tuple']
+__all__ = [
+    'polysplit_main',
+    'generate_points_within_polygon',
+    'shortest_path_distance',
+    'plot_polygon_and_regions',
+    'shapely_point_to_tuple',
+]
