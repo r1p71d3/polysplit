c934b776ff514be4b49f3d8ae54f067bb538c64a
r1p71d3 <dz2428@columbia.edu>
2023-04-23 18:03:15 -0400

    black and flake8

--- polysplit/tests/test_polysplit.py
+++ polysplit/tests/test_polysplit.py
@@ -24,7 +24,17 @@ class TestPolySplit(unittest.TestCase):
         point1 = (0.0, 0.0)
         point2 = (1.0, 1.0)
         expected_result = 1.41421356  # square root of 2
-        result = ps.shortest_path_distance(point1, point2)
+        result = ps.euclidean_distance(point1, point2)
+        self.assertAlmostEqual(result, expected_result, places=5)
+
+    def test_visibility(self):
+        outer_coords = [(0, 0), (0, 1), (1, 1), (1, 0)]
+        hole_coords = [(0.4, 0.4), (0.4, 0.6), (0.6, 0.6), (0.6, 0.4)]
+        polygon = Polygon(outer_coords, [hole_coords])
+        p1 = (0.0, 0.0)
+        p2 = (1.0, 1.0)
+        expected_result = 1.44222051
+        result = ps.visibility(polygon, p1, p2)[1]
         self.assertAlmostEqual(result, expected_result, places=5)
 
     def test_polysplit(self):
