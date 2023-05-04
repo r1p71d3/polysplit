d96c12a09a6e4509ce141ea4dd22981a099ca3fa
Dmitrii Zakharov <91263222+r1p71d3@users.noreply.github.com>
2023-05-04 15:24:28 -0400

    Setup fix (#14)
    * fixed setup.py
    
    * version update
    
    * flake8 setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,19 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-
-setup()
+setup(
+    name='polysplit',
+    version='0.1.2',
+    packages=find_packages(),
+    install_requires=[
+        'shapely',
+        'numpy',
+        'scikit-learn-extra',
+        'matplotlib',
+        'networkx',
+    ],
+    extras_require={
+        'tests': [
+            'unittest',
+        ],
+    },
+)
